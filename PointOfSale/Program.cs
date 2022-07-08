1f2b0f6a9c4f45ca62f4324a20a4624ddceac6ca
Travis <travisamador@gmail.com>
2022-07-08 16:13:30 -0400

    added add to cart interaction

--- PointOfSale/Program.cs
+++ PointOfSale/Program.cs
@@ -5,7 +5,7 @@
 //desks, oven, pillows, blankets, tv stand, bunk beds
 //decorative rocks, cinnamonbuns, shrooms, shoe strings
 List<Product> items = new List<Product>();
-List < Product > addToCart = new List < Product > ();
+
 double subTotal = 0;
 double salesTax = .06;
 double grandTotal = 0;
@@ -32,14 +32,28 @@ while (true)
 //when finished close it 
 reader.Close();
 
-Product.Inventory(items);
 
 
-//
+static List<Product> addToCart(List<Product> list)
+{
+    List<Product> itemList = new List<Product>();
+    Product.Inventory(list); // display items
+    int choice = Validator.Validator.GetUserNumberInt("What product would you like to add to your cart?");
+    itemList.Add(list[choice - 1]); //addToList needs to be implemented
+    Console.WriteLine($"You have chosen: ");
+
+    return itemList;
+}
+
+
+
+List<Product> Cart = addToCart(items);
+Product.Inventory(Cart);
+
 //static double subtotals()
 //{
-//    Product.Inventory(); // display items
-//    //Product choice; = new Product.addToList(); //addToList needs to be implemented
+
+
 //    Console.WriteLine("How many would you like?");
 //    double userInput = int.Parse(Console.ReadLine());
 //    double subtotal = 0;
@@ -50,24 +64,9 @@ Product.Inventory(items);
 //    return userInput;
 //}
 
-static void Tender(double grandTotal)
-{
-    //tender
-    Console.WriteLine("How much are you paying with?");
-    double tender = double.Parse(Console.ReadLine());
-    if (tender >= grandTotal)
-    {
-        Console.WriteLine($"Thanks, your change is ${Math.Round(tender - grandTotal, 2)} ");
-    }
-    else if (tender < grandTotal)
-    {
-        Console.WriteLine($"Thats not enough, {Math.Round(grandTotal - tender, 2)}");
-    }
-    else
-    {
-        Console.WriteLine("Invalid, try again.");
-    }
-}
+
+
+
 
 
 
---
+++ PointOfSale/Validator.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+    internal class Validator
+    {
+        //Checks if value is between min and max
+        public static bool InRange(int value, int min, int max)
+        {
+            if(value >= min && value <= max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool InRange(double value, double min, double max)
+        {
+            if (value >= min && value <= max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool InRange(float value, float min, float max)
+        {
+            if (value >= min && value <= max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool InRange(decimal value, decimal min, decimal max)
+        {
+            if (value >= min && value <= max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Return back a number
+        public static int GetUserNumberInt(string message)
+        {
+            int result = 0;
+            Console.WriteLine(message);
+            //if parse fails, get input again
+            while(!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Invalid input");
+            }
+            //input is now valid
+            return result;
+        }
+
+        public static double GetUserNumberDouble()
+        {
+            double result = 0;
+            Console.WriteLine("Please enter a number");
+            //if parse fails, get input again
+            while (!double.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Invalid input");
+            }
+            //input is now valid
+            return result;
+        }
+        public static float GetUserNumberFloat()
+        {
+            float result = 0;
+            Console.WriteLine("Please enter a number");
+            //if parse fails, get input again
+            while (!float.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Invalid input");
+            }
+            //input is now valid
+            return result;
+        }
+
+        public static decimal GetUserNumberDecimal()
+        {
+            decimal result = 0;
+            Console.WriteLine("Please enter a number");
+            //if parse fails, get input again
+            while (!decimal.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Invalid input");
+            }
+            //input is now valid
+            return result;
+        }
+
+        public static bool GetContinue()
+        {
+            //default value
+            bool result = true;
+            //check if user wants to continue
+            while (true)
+            {
+                Console.WriteLine("Would you like to continue? y/n");
+                string choice = Console.ReadLine().Trim().ToLower();
+
+                //Check user input
+                if(choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if(choice == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That was invalid. Try again.");
+                }
+            }
+            return result;
+        }
+
+        //Overloaded. Allows for custom message
+        public static bool GetContinue(string message)
+        {
+            //default value
+            bool result = true;
+            //check if user wants to continue
+            while (true)
+            {
+                Console.WriteLine($"{message} y/n");
+                string choice = Console.ReadLine().Trim().ToLower();
+
+                //Check user input
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That was invalid. Try again.");
+                }
+            }
+            return result;
+        }
+
+        public static bool GetContinue(string message, string yes, string no)
+        {
+            //default value
+            bool result = true;
+            //check if user wants to continue
+            while (true)
+            {
+                Console.WriteLine($"{message} {yes}/{no}");
+                string choice = Console.ReadLine().Trim().ToLower();
+
+                //Check user input
+                if (choice == yes.ToLower())
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == no.ToLower())
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That was invalid. Try again.");
+                }
+            }
+            return result;
+        }
+
+
+
+    }
+}
