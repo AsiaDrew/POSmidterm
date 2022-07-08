0ee95cd68a0d806d9ea8185d01752da9ac28a45c
AsiaDrew <asiadrew2@outlook.com>
2022-07-08 14:12:36 -0400

    second commit

--- PointOfSale/Program.cs
+++ PointOfSale/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PointOfSale;
+
+//12 items
+//meatball, coffee, hotdog, shelves, lamps, end tables
+//desks, oven, pillows, blankets, tv stand, bunk beds
+//decorative rocks, cinnamonbuns, shrooms, shoe strings
+List<Product> items = new List<Product>();
+string filePath = "../../../Products.txt";
+StreamReader reader = new StreamReader(filePath);
+
+
+while (true)
+{
+    string line = reader.ReadLine();
+    if (line == null)//it pulled out current student and found nothing
+    {
+        break;
+    }
+    else
+    {
+        string[] values = line.Split(',');
+        Product newProduct = new Product();
+        items.Add(newProduct);
+    }
+}
+
+//when finished close it 
+reader.Close();
+
+//MAIN PROGRAM
+ static double GetItem(double productCount)
+{
+    Console.WriteLine("How many would you like?");
+    double userInput = double.Parse(Console.ReadLine());
+    for (int i = 0; i < productCount; i++)
+    {
+        userInput++;
+    }
+}
